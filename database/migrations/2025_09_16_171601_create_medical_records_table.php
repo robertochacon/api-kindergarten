5d87c307825cb9322f1272239228587fad550725
rchaconrv <hola.hack09@gmail.com>
2025-09-16 17:58:31 -0400

    Improvements

--- database/migrations/2025_09_16_171601_create_medical_records_table.php
+++ database/migrations/2025_09_16_171601_create_medical_records_table.php
@@ -15,87 +15,87 @@ return new class extends Migration
             $table->id();
             $table->foreignId('kid_id')->constrained('kids')->onDelete('cascade');
             
-            // ANTECEDENTES PRENATALES
-            $table->boolean('bajo_riesgo')->nullable();
-            $table->boolean('alto_riesgo')->nullable();
+            // PRENATAL HISTORY
+            $table->boolean('low_risk')->nullable();
+            $table->boolean('high_risk')->nullable();
             $table->boolean('preeclampsia')->nullable();
-            $table->boolean('sangrados')->nullable();
-            $table->boolean('infecciones')->nullable();
-            $table->boolean('madre_adolescente')->nullable();
-            $table->boolean('rn_a_termino')->nullable();
-            $table->boolean('rn_prematuro')->nullable();
-            $table->string('semanas_al_nacer')->nullable();
+            $table->boolean('bleeding')->nullable();
+            $table->boolean('infections')->nullable();
+            $table->boolean('teenage_mother')->nullable();
+            $table->boolean('full_term_newborn')->nullable();
+            $table->boolean('premature_newborn')->nullable();
+            $table->string('weeks_at_birth')->nullable();
             
-            // NACIMIENTO
-            $table->boolean('parto')->nullable();
-            $table->boolean('cesarea')->nullable();
-            $table->boolean('lloro_al_nacer')->nullable();
-            $table->string('peso_al_nacer')->nullable();
-            $table->string('talla')->nullable();
-            $table->string('perimetro_cefalico')->nullable();
-            $table->boolean('complicacion_al_nacer')->nullable();
-            $table->text('descripcion_complicacion')->nullable();
-            $table->string('tiempo_hospitalizado')->nullable();
-            $table->boolean('incubadora')->nullable();
-            $table->boolean('oxigeno')->nullable();
-            $table->boolean('ventilacion_mecanica')->nullable();
+            // BIRTH
+            $table->boolean('vaginal_delivery')->nullable();
+            $table->boolean('cesarean_delivery')->nullable();
+            $table->boolean('cried_at_birth')->nullable();
+            $table->string('birth_weight')->nullable();
+            $table->string('birth_length')->nullable();
+            $table->string('head_circumference')->nullable();
+            $table->boolean('birth_complications')->nullable();
+            $table->text('complications_description')->nullable();
+            $table->string('hospitalization_time')->nullable();
+            $table->boolean('incubator')->nullable();
+            $table->boolean('oxygen_therapy')->nullable();
+            $table->boolean('mechanical_ventilation')->nullable();
             
-            // DESARROLLO Y NUTRICION
-            $table->boolean('lactancia_materna_al_nacer')->nullable();
-            $table->string('duracion_lactancia')->nullable();
-            $table->string('inicio_alimentacion_complementaria')->nullable();
-            $table->boolean('suplemento_hierro')->nullable();
-            $table->boolean('suplemento_vitamina_a')->nullable();
-            $table->boolean('suplemento_multivitaminas')->nullable();
-            $table->boolean('estimulantes_apetito')->nullable();
-            $table->boolean('come_solo')->nullable();
-            $table->string('cantidad_veces_come')->nullable();
-            $table->boolean('buen_apetito')->nullable();
-            $table->boolean('alergia_alimento')->nullable();
+            // DEVELOPMENT AND NUTRITION
+            $table->boolean('breastfeeding_at_birth')->nullable();
+            $table->string('breastfeeding_duration')->nullable();
+            $table->string('complementary_feeding_start')->nullable();
+            $table->boolean('iron_supplement')->nullable();
+            $table->boolean('vitamin_a_supplement')->nullable();
+            $table->boolean('multivitamin_supplement')->nullable();
+            $table->boolean('appetite_stimulants')->nullable();
+            $table->boolean('eats_independently')->nullable();
+            $table->string('eating_frequency')->nullable();
+            $table->boolean('good_appetite')->nullable();
+            $table->boolean('food_allergy')->nullable();
             
-            // ASPECTOS PSICOMOTORES
-            $table->boolean('controla_esfinteres')->nullable();
-            $table->boolean('utiliza_panal')->nullable();
-            $table->boolean('discapacidad_diagnosticada')->nullable();
-            $table->text('descripcion_discapacidad')->nullable();
-            $table->text('antecedentes_patologicos')->nullable();
-            $table->boolean('alergico_medicamento')->nullable();
-            $table->text('medicamento_permanente')->nullable();
-            $table->boolean('enfermedades_previas')->nullable();
-            $table->text('tipo_enfermedad')->nullable();
-            $table->boolean('estado_ingresado')->nullable();
-            $table->string('veces_ingresado')->nullable();
+            // PSYCHOMOTOR ASPECTS
+            $table->boolean('controls_sphincters')->nullable();
+            $table->boolean('uses_diapers')->nullable();
+            $table->boolean('diagnosed_disability')->nullable();
+            $table->text('disability_description')->nullable();
+            $table->text('pathological_history')->nullable();
+            $table->boolean('medication_allergy')->nullable();
+            $table->text('permanent_medication')->nullable();
+            $table->boolean('previous_diseases')->nullable();
+            $table->text('disease_type')->nullable();
+            $table->boolean('hospitalized')->nullable();
+            $table->string('times_hospitalized')->nullable();
             $table->boolean('anemia')->nullable();
-            $table->boolean('fiebre')->nullable();
-            $table->boolean('infecciones_previas')->nullable();
-            $table->boolean('deshidratacion')->nullable();
-            $table->boolean('luce_desnutrido')->nullable();
-            $table->boolean('accidentes')->nullable();
-            $table->boolean('cirugias')->nullable();
+            $table->boolean('fever')->nullable();
+            $table->boolean('previous_infections')->nullable();
+            $table->boolean('dehydration')->nullable();
+            $table->boolean('appears_malnourished')->nullable();
+            $table->boolean('accidents')->nullable();
+            $table->boolean('surgeries')->nullable();
             
-            // EXAMEN FISICO
-            $table->text('cabeza')->nullable();
-            $table->text('fontanelas')->nullable();
-            $table->text('cara')->nullable();
-            $table->text('ojos_oidos_nariz')->nullable();
-            $table->text('boca')->nullable();
-            $table->text('cuello')->nullable();
-            $table->text('torax')->nullable();
+            // PHYSICAL EXAMINATION
+            $table->text('head')->nullable();
+            $table->text('fontanelles')->nullable();
+            $table->text('face')->nullable();
+            $table->text('eyes_ears_nose')->nullable();
+            $table->text('mouth')->nullable();
+            $table->text('neck')->nullable();
+            $table->text('chest')->nullable();
             $table->text('abdomen')->nullable();
-            $table->text('extremidades_superiores_inferiores')->nullable();
-            $table->text('genitourinario')->nullable();
-            $table->text('piel')->nullable();
-            $table->string('peso_lbs')->nullable();
-            $table->string('talla_cms')->nullable();
-            $table->string('pliegue_brazo')->nullable();
-            $table->string('imc')->nullable();
-            $table->string('estado_nutricional')->nullable();
+            $table->text('upper_lower_extremities')->nullable();
+            $table->text('genitourinary')->nullable();
+            $table->text('skin')->nullable();
+            $table->string('current_weight_lbs')->nullable();
+            $table->string('current_height_cms')->nullable();
+            $table->string('arm_fold_measurement')->nullable();
+            $table->string('bmi')->nullable();
+            $table->string('nutritional_status')->nullable();
             
-            // DATOS DEL PEDIATRA O MEDICO EVALUADOR
-            $table->string('firma_medico')->nullable();
-            $table->string('exequatur')->nullable();
-            $table->string('cmd')->nullable();
-            $table->text('tabla_crecimiento_desarrollo')->nullable();
+            // DOCTOR DATA
+            $table->string('doctor_signature')->nullable();
+            $table->string('medical_license')->nullable();
+            $table->string('medical_college_registration')->nullable();
+            $table->text('growth_development_chart')->nullable();
             
             $table->timestamps();
         });
